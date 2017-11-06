cf437f79882862dbb78fc0824b5b73c1e80dc824
Loïc Droz <lk.droz@gmail.com>
2017-11-06 16:17:40 +0100

    Config pins for CLK out, in and DATA in.

--- testing/1Mic-CIC-Filter/deploy.sh
+++ testing/1Mic-CIC-Filter/deploy.sh
@@ -3,6 +3,15 @@
 set -e
 
 # TODO: configure the pins
+## CLK output pin from PRU0
+config-pin -a P8.11 pruout
+config-pin -q P8.11
+## CLK input pin to PRU1
+config-pin -a P8.45 pruin
+config-pin -q P8.45
+## DATA input pin to PRU1
+config-pin -a P8.46 pruin
+config-pin -q P8.46
 
 # Build the program
 make clean
