56d956a934eb2038036edf54cafdfc15fa0889b2
Loïc Droz <lk.droz@gmail.com>
2017-11-09 12:14:37 +0000

    Add echoes and make directory gen/

--- testing/1Mic-CIC-Filter/deploy.sh
+++ testing/1Mic-CIC-Filter/deploy.sh
@@ -3,16 +3,20 @@
 set -e
 
 ## CLK output pin from PRU0
+echo "CLK out (PRU0)"
 config-pin -a P8.11 pruout
 config-pin -q P8.11
 ## DATA input pin to PRU1
+echo "DATA in (PRU1)"
 config-pin -a P8.28 pruin
 config-pin -q P8.28
 ## CLK input pin to PRU1
+echo "CLK in (PRU1)"
 config-pin -a P8.30 pruin
 config-pin -q P8.30
 
 # Build the program
+mkdir -p gen
 make clean
 make
 
