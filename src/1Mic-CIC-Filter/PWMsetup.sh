03009dc352142143d0e9beec2298b47a7715f3e3
Loïc Droz <lk.droz@gmail.com>
2017-11-16 14:54:00 +0100

    Correct bad variable call (again).

--- src/1Mic-CIC-Filter/PWMsetup.sh
+++ src/1Mic-CIC-Filter/PWMsetup.sh
@@ -8,6 +8,6 @@ PWM_DUTY_CYCLE='400'
 cd /sys/class/pwm/pwmchip3/
 echo 0 > export
 # Setting up parameters for PWM and enable it
-echo PWM_PERIOD_NS > period
-echo PWM_DUTY_CYCLE > duty_cycle
+echo $PWM_PERIOD_NS > period
+echo $PWM_DUTY_CYCLE > duty_cycle
 echo 1 > enable
