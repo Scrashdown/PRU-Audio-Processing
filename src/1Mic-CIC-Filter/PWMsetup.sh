6037d2c03cfde75d2e07def6810603d364290967
Loïc Droz <lk.droz@gmail.com>
2017-11-16 14:56:05 +0100

    Debug info.

--- src/1Mic-CIC-Filter/PWMsetup.sh
+++ src/1Mic-CIC-Filter/PWMsetup.sh
@@ -2,12 +2,19 @@
 # Abort on first error
 set -e
 
+echo "Setting up BeagleBone Black PWM (CLK)"
+
 PWM_PERIOD_NS='800'
 PWM_DUTY_CYCLE='400'
 
 cd /sys/class/pwm/pwmchip3/
+echo "1"
 echo 0 > export
 # Setting up parameters for PWM and enable it
+echo "2"
 echo $PWM_PERIOD_NS > period
+echo "3"
 echo $PWM_DUTY_CYCLE > duty_cycle
+echo "4"
 echo 1 > enable
+echo "finish"
