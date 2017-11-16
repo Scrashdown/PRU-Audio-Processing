6204ebcbcd9f477d21b04564a3dde8b47eae1c89
Loïc Droz <lk.droz@gmail.com>
2017-11-16 14:48:32 +0100

    Move the PWM setup script to a new file.

---
+++ src/1Mic-CIC-Filter/PWMsetup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Abort on first error
+set -e
+
+PWM_PERIOD_NS='800'
+PWM_DUTY_CYCLE='400'
+
+cd /sys/class/pwm/pwmchip3/
+echo 0 > export
+# Setting up parameters for PWM and enable it
+echo $(PWM_PERIOD_NS) > period
+echo $(PWM_DUTY_CYCLE) > duty_cycle
+echo 1 > enable
--- src/1Mic-CIC-Filter/deploy.sh
+++ src/1Mic-CIC-Filter/deploy.sh
@@ -6,17 +6,7 @@ set -e
 echo "PWM/CLK output (BeagleBone)"
 config-pin -a P9.14 pwm
 config-pin -q P9.14
-sudo su
-    PWM_PERIOD_NS='800'
-    PWM_DUTY_CYCLE='400'
-
-    cd /sys/class/pwm/pwmchip3/
-    echo 0 > export
-    # Setting up parameters for PWM and enable it
-    echo $(PWM_PERIOD_NS) > period
-    echo $(PWM_DUTY_CYCLE) > duty_cycle
-    echo 1 > enable
-exit
+sudo sh "PWMsetup.sh"
 
 ## DATA input pin to PRU1
 echo "DATA in (PRU1)"
