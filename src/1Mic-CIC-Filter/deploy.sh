5dca9a559f0618d745fb373805fa5737aaf28efc
Loïc Droz <lk.droz@gmail.com>
2017-11-16 14:16:31 +0100

    Remove PRU0 CLK and setup PWM.

--- src/1Mic-CIC-Filter/deploy.sh
+++ src/1Mic-CIC-Filter/deploy.sh
@@ -2,10 +2,22 @@
 # Stop on first error
 set -e
 
-## CLK output pin from PRU0
-echo "CLK out (PRU0)"
-config-pin -a P8.11 pruout
-config-pin -q P8.11
+PWM_PERIOD_NS = 800
+PWM_DUTY_CYCLE = 400
+
+## Setup pin P9.14 for PWM (CLK) output
+echo "PWM/CLK output (BeagleBone)"
+config-pin -a P9.14 PWM
+config-pin -q P9.14
+sudo su
+    cd /sys/class/pwm/pwmchip3/
+    echo 0 > export
+    # Setting up parameters for PWM and enable it
+    echo $(PWM_PERIOD_NS) > period
+    echo $(PWM_DUTY_CYCLE) > duty_cycle
+    echo 1 > enable
+exit
+
 ## DATA input pin to PRU1
 echo "DATA in (PRU1)"
 config-pin -a P8.28 pruin
@@ -27,4 +39,4 @@ make
 
 # Start the program
 cd gen
-./loader clk_pru0.bin cic_pru1.bin
+./loader cic_pru1.bin
