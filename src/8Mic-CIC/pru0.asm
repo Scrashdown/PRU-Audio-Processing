b2c6ad35a9c784ff448eb89e130f12b96abd44cc
Loïc Droz <lk.droz@gmail.com>
2017-11-28 17:08:51 +0000

    Crude implementation. Need to test it and find a way to output the results.

--- src/8Mic-CIC/pru0.asm
+++ src/8Mic-CIC/pru0.asm
@@ -35,18 +35,22 @@ http://processors.Wiki.ti.com/index.php/PRU_Assembly_Instructions
 #define LAST_COMB1_CHAN12 r9
 #define LAST_COMB2_CHAN12 r10
 
-#define INT0_CHAN34 r11
-#define INT1_CHAN34 r12
-#define INT2_CHAN34 r13
-#define INT3_CHAN34 r14
-#define LAST_INT_CHAN34 r15
+#define OUTPUT_CHAN12 r11
 
-#define COMB0_CHAN34 r16
-#define COMB1_CHAN34 r17
-#define COMB2_CHAN34 r18
-#define LAST_COMB0_CHAN34 r19
-#define LAST_COMB1_CHAN34 r20
-#define LAST_COMB2_CHAN34 r21
+#define INT0_CHAN34 r12
+#define INT1_CHAN34 r13
+#define INT2_CHAN34 r14
+#define INT3_CHAN34 r15
+#define LAST_INT_CHAN34 r16
+
+#define COMB0_CHAN34 r17
+#define COMB1_CHAN34 r18
+#define COMB2_CHAN34 r19
+#define LAST_COMB0_CHAN34 r20
+#define LAST_COMB1_CHAN34 r21
+#define LAST_COMB2_CHAN34 r22
+
+#define OUTPUT_CHAN34 r23
 
 #define IN_PINS r31
 #define TMP_REG r29
@@ -76,33 +80,8 @@ http://processors.Wiki.ti.com/index.php/PRU_Assembly_Instructions
 #define SET_LED SET r30, r30, 15
 #define CLR_LED CLR r30, r30, 15
 
-.origin 0
-.entrypoint start
-
-start:
-    CLR_LED
-
-    // ### Setup start configuration ###
-    // Set all register values to zero, except r31
-    // Make sure bank 1 is also set to 0
-    ZERO    0, 124
-    XOUT    BANK1, r0, 124
-
-    // ##### CHANNELS 1 - 4 #####
-wait_rising_edge:
-    // First wait for CLK = 0, then wait for CLK = 1
-    WBC     IN_PINS, CLK_OFFSET
-    WBS     IN_PINS, CLK_OFFSET
-
-    // Wait for t_dv time, since it can be at most 125ns, we have to wait for 25 cycles
-    LDI     TMP_REG, 12
-wait_data:
-    SUB     TMP_REG, TMP_REG, 1
-    QBNE    wait_data, TMP_REG, 0
-
-    // Update sample counter for decimation
-    ADD     SAMPLE_COUNTER, SAMPLE_COUNTER, 1
-
+// Macros for integrator and comb stages
+.macro integrators
     // Retrieve data from input pins and perform the 4 first stages of the integrators
     // Stage 0 / 3
     LSR     TMP_REG, IN_PINS, DAT_OFFSET1
@@ -137,11 +116,10 @@ wait_data:
     ADD     INT3_CHAN12.W2, INT3_CHAN12.W2, INT2_CHAN12.W2  // Channel 2
     ADD     INT3_CHAN34.W0, INT3_CHAN34.W0, INT2_CHAN34.W0  // Channel 3
     ADD     INT3_CHAN34.W2, INT3_CHAN34.W2, INT2_CHAN34.W2  // Channel 4
+.endm
 
-    // TODO: Branch for oversampling, if ov. rate reached, execute comb stages
-    QBNE    wait_falling_edge, SAMPLE_COUNTER, 64
-
-    // ##### 27 / 53 cycles
+.macro combs
+    // ##### 27 / 72 cycles (at 16kHz)
     // Perform comb stages, update channels separately
     // Stage 0 / 3
     SUB     COMB0_CHAN12.W0, INT3_CHAN12.W0, LAST_INT_CHAN12.W0  // Channel 1
@@ -159,12 +137,12 @@ wait_data:
     SUB     COMB2_CHAN34.W0, COMB1_CHAN34.W0, LAST_COMB1_CHAN34.W0  // Channel 3
     SUB     COMB2_CHAN34.W2, COMB1_CHAN34.W2, LAST_COMB1_CHAN34.W2  // Channel 4
     // Stage 3 / 3
-    SUB     TMP_REG.W0, COMB2_CHAN12.W0, LAST_COMB2_CHAN12.W0  // Channel 1
-    SUB     TMP_REG.W2, COMB2_CHAN12.W2, LAST_COMB2_CHAN12.W2  // Channel 2
-    // TODO: output the result somewhere...
-    SUB     TMP_REG.W0, COMB2_CHAN34.W0, LAST_COMB2_CHAN34.W0  // Channel 3
-    SUB     TMP_REG.W2, COMB2_CHAN34.W2, LAST_COMB2_CHAN34.W2  // Channel 4
-    // TODO: output the result somewhere...
+    SUB     OUTPUT_CHAN12.W0, COMB2_CHAN12.W0, LAST_COMB2_CHAN12.W0  // Channel 1
+    SUB     OUTPUT_CHAN12.W2, COMB2_CHAN12.W2, LAST_COMB2_CHAN12.W2  // Channel 2
+    // The result will be written to BANK 0 and taken care of by PRU1
+    SUB     OUTPUT_CHAN34.W0, COMB2_CHAN34.W0, LAST_COMB2_CHAN34.W0  // Channel 3
+    SUB     OUTPUT_CHAN34.W2, COMB2_CHAN34.W2, LAST_COMB2_CHAN34.W2  // Channel 4
+    // The result will be written to BANK 0 and taken care of by PRU1
 
     // Update comb values
     MOV     LAST_INT_CHAN12, INT3_CHAN12
@@ -175,11 +153,75 @@ wait_data:
     MOV     LAST_COMB1_CHAN34, COMB1_CHAN34
     MOV     LAST_COMB2_CHAN12, COMB2_CHAN12
     MOV     LAST_COMB2_CHAN34, COMB2_CHAN34
+.endm
 
+.origin 0
+.entrypoint start
+
+start:
+    CLR_LED
+
+    // ### Setup start configuration ###
+    // Set all register values to zero, except r31
+    // Make sure bank 1 is also set to 0
+    ZERO    0, 124
+    XOUT    BANK1, r0, 124
+
+    // ##### CHANNELS 1 - 4 #####
+chan1to4:
+    // Swap registers for processing channels 1 - 4
+    // Store registers for chan. 5 - 8 in bank 1
+    XOUT    BANK1, r0, 96
+    // Load registers for chan. 5 - 8 from bank 0
+    XIN     BANK0, r0, 96
+
+    // Wait for rising edge
+    WBC     IN_PINS, CLK_OFFSET
+    WBS     IN_PINS, CLK_OFFSET
+
+    // Wait for t_dv time, since it can be at most 125ns, we have to wait for 25 cycles
+    LDI     TMP_REG, 11
+wait_data1:
+    SUB     TMP_REG, TMP_REG, 1
+    QBNE    wait_data1, TMP_REG, 0
+
+    // Update sample counter for decimation
+    ADD     SAMPLE_COUNTER, SAMPLE_COUNTER, 1
+
+    // Integrator stages
+    integrators  // 24 cycles
+
+    QBNE    chan5to8, SAMPLE_COUNTER, 64
+
+    // Comb stages
+    combs  // 24 cycles
+
+    // ##### Channels 5 - 8 #####
+chan5to8:
     // Swap registers for processing channels 5 - 8
     // Store registers for chan. 1 - 4 in bank 0
-    XOUT    BANK0, r0, 44  // 44 = 4 * 11, we use 11 registers (r0 - r10) to store channels 1 - 4 data
+    XOUT    BANK0, r0, 96  // 96B = 24 * 4B = 24 registers that we use to store data for 4 channels
     // Load registers for chan. 5 - 8 from bank 1
-    XIN     BANK1, r11, 44  // Same, but for registers (r11 - r21) for channels 5 - 8
+    XIN     BANK1, r0, 96  // Same idea
 
-    // TODO: reset sample counter to 0 at the end of we reached decimation rate
+    // Wait for falling edge
+    WBS     IN_PINS, CLK_OFFSET
+    WBC     IN_PINS, CLK_OFFSET
+
+    // Wait for t_dv time, since it can be at most 125ns, we have to wait for 25 cycles
+    LDI     TMP_REG, 11
+wait_data2:
+    SUB     TMP_REG, TMP_REG, 1
+    QBNE    wait_data2, TMP_REG, 0
+
+    // Integrator stages
+    integrators  // 24 cycles
+
+    QBNE    chan1to4, SAMPLE_COUNTER, 64
+    LDI     SAMPLE_COUNTER, 0  // Reset counter to 0 this time
+
+    // Comb stages...
+    combs  // 24 cycles
+
+    // Go back to computing channels 1 - 4
+    QBA     chan1to4
