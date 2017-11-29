70cae7f96f04ac6fc688930dc05647e4fcff4240
Loïc Droz <lk.droz@gmail.com>
2017-11-29 10:56:50 +0100

    Adjust PWM values.

--- src/8Mic-CIC/deploy.sh
+++ src/8Mic-CIC/deploy.sh
@@ -7,8 +7,8 @@ echo "PWM/CLK output (BeagleBone)"
 config-pin -a P9.14 pwm
 config-pin -q P9.14
 
-PWM_PERIOD_NS='968'
-PWM_DUTY_CYCLE='484'
+PWM_PERIOD_NS='971'
+PWM_DUTY_CYCLE='485'
 cat "../utils/PWMsetup.sh" | sudo bash -s $PWM_PERIOD_NS $PWM_DUTY_CYCLE
 
 ## DATA input pins to PRU1
