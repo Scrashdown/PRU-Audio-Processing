912f34d7f51a585edb774a877281f28f3788d6d5
Loïc Droz <lk.droz@gmail.com>
2017-12-07 12:10:13 +0100

    Fix bug with reshaping.

--- src/6Mic-CIC/wav_conv/PCMtoWAV.py
+++ src/6Mic-CIC/wav_conv/PCMtoWAV.py
@@ -8,17 +8,17 @@ in_file = "../output/32bits_6chan.pcm"
 out_file = "../output/32bits_6chan.wav"
 
 # FIXME: there is currently a spike at the beginning of the recording, cut it off for now
-offset = N
+offset = 6 * 4
 
 # Get the raw data to a numpy u32 array
 raw_u32 = np.fromfile(in_file, dtype=np.uint32, count=-1, sep='')[offset:]
 number_samples = int(raw_u32.shape[0] / 6)
 reshaped_arr = np.reshape(raw_u32, (number_samples, 6))
 
-first_channel = reshaped_arr[:, 0]
+channel = reshaped_arr[:, 0]
 
 # Convert the raw data to float32 between 1.0 and -1.0
-temp = np.subtract(first_channel, (first_channel.max() + first_channel.min()) / 2)
+temp = np.subtract(channel, (channel.max() + channel.min()) / 2)
 norm_f32 = np.divide(temp, max(abs(temp)))
 
 plt.figure()
