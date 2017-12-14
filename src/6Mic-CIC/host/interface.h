5eed55195a910c67f9daf2059b3eeb52900611f3
Loïc Droz <lk.droz@gmail.com>
2017-12-14 15:37:23 +0100

    Initial commit.

---
+++ src/6Mic-CIC/host/interface.h
@@ -0,0 +1,46 @@
+/**
+ * Interface of the project.
+ * 
+ */
+
+#include <stddef.h>
+#include "ringbuffer.h"
+
+struct pcm_t {
+    // Number of channels
+    size_t n_chan;
+    // *Per-channel* sample rate of the PCM signal in Hz
+    size_t sample_rate;
+    // The ring buffer the PCM is bound to
+    ringbuffer_t * buffer;
+};
+
+/**
+ * 
+ * 
+ */
+int pru_processing_init(pcm_t * pcm, size_t n_chan, size_t sample_rate);
+
+/**
+ * 
+ * 
+ */
+int pru_processing_close();
+
+/**
+ * Read a given number of blocks of given size and output them to the user provided buffer.
+ * 
+ */
+size_t pcm_read(pcm_t * pcm, void * buffer, size_t blocksize, size_t nblocks);
+
+/**
+ * 
+ * 
+ */
+int enable_recording();
+
+/**
+ * 
+ * 
+ */
+void disable_recording();
---
+++ src/6Mic-CIC/host/ringbuffer.h
@@ -0,0 +1,36 @@
+/**
+ * Ringbuffer of buffers, each of a constant size.
+ * 
+ */
+
+#include <stddef.h>
+
+struct ringbuffer_t {
+
+};
+
+typedef struct ringbuffer_t ringbuffer_t;
+
+/**
+ * TODO:
+ * 
+ */
+ringbuffer_t * ringbuf_init();
+
+/**
+ * 
+ * 
+ */
+void ringbuf_free(ringbuffer_t * ringbuf);
+
+/**
+ * Push one frame (= 1 half PRU buffer) to the buffer.
+ * 
+ */
+int ringbuf_push_frame(const void * src, ringbuffer_t * dst);
+
+/**
+ * Pop a given number of elemnts. TODO: decide what elements are.
+ * 
+ */
+size_t ringbuf_pop(const ringbuffer_t * src, void * dest, size_t nbelem);
