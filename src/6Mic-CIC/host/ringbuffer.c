385d7376c208dd1e6c05ee8824729406d85dd4bb
Loïc Droz <lk.droz@gmail.com>
2017-12-21 15:49:19 +0100

    Update ringbuf_pop.

--- src/6Mic-CIC/host/ringbuffer.c
+++ src/6Mic-CIC/host/ringbuffer.c
@@ -34,15 +34,15 @@ ringbuffer_t * ringbuf_create(size_t nelem, size_t blocksize)
     return ringbuf;
 }
 
-// TODO: need to make sure we do not partially write a set of samples (6 * 4 bytes) in case of an overflow
+
 size_t ringbuf_push(ringbuffer_t * dst, uint8_t * data, size_t block_size, size_t block_count)
 {
     // Get the distance between the tail and head pointers, and check we aren't trying to push too much data.
     size_t free_bytes;
     if (dst -> head >= dst -> tail) {
-        free_bytes = dst -> maxLength - dst -> head + dst -> tail;
+        free_bytes = (dst -> maxLength) - (dst -> head) + (dst -> tail);
     } else {
-        free_bytes = dst -> tail - dst -> head;
+        free_bytes = (dst -> tail) - (dst -> head);
     }
 
     // Write only the maximum amount of data possible without overwriting.
@@ -61,18 +61,23 @@ size_t ringbuf_push(ringbuffer_t * dst, uint8_t * data, size_t block_size, size_
 
 size_t ringbuf_pop(ringbuffer_t * src, uint8_t * data, size_t block_size, size_t block_count)
 {
-     // If the head isn't ahead of the tail, nothing to read
-     // TODO: could implement blocking functionality
-     if (src -> head == src -> tail) {
-         return -1;
-     }
+    // Get the distance between the tail and head pointers, check we aren't trying to pop too much data.
+    size_t available_bytes;
+    if (src -> head >= src -> tail) {
+        available_bytes = (src -> head) - (src -> tail);
+    } else {
+        available_bytes = (src -> maxLength) - (src -> tail) + (src -> head);
+    }
 
-     // Next is where tail will point after reading one byte
-     size_t next = (src -> tail + 1) % (src -> maxLength);
+    // Read only the maximum amount of data possible without underflow.
+    size_t to_read = (block_size * block_count) > available_bytes ? available_bytes : (block_size * block_count);
+    // Copy the data
+    memcpy((const void *) data, &(src -> data[src -> tail]), to_read);
 
-     *data = src -> data[src -> tail];
-     src -> tail = next;
-     return 0;
+    // Adjust tail pointer
+    src -> tail += to_read;
+    src -> tail %= src -> maxLength;
+    return  to_read;
 }
 
 
