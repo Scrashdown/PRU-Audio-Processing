53da3e8cf9dbcf364245d91a5b0ee97a6582df27
Loïc Droz <lk.droz@gmail.com>
2018-01-08 15:14:39 +0100

    Add computation of the required bit width.

--- utils/power_resp_CIC.py
+++ utils/power_resp_CIC.py
@@ -20,14 +20,18 @@ def P_hat(N, M, R, fs):
     # drop DC value since NaN due to division by 0
     return P[1:], P_est[1:], freqs_hz[1:]  
 
+def bit_width(N, M, R):
+
+    return np.ceil(1 + np.log2(R*M) * N)
 
 if __name__ == "__main__":
 
-    N = 4
+    N = 5
     M = 1
-    R = 16
+    R = 64
     fs = 64 * 16e3
     P, P_est, freqs_hz = P_hat(N,M,R,fs)
+    B = bit_width(N, M, R)
 
     # convert to dB and normalize so we get attenuation
     P_db = 10*np.log10(np.abs(P))
@@ -50,7 +54,7 @@ if __name__ == "__main__":
     # add plot of f_c line
 
     plt.tight_layout()
-    plt.title('N=%d, M=%d, R=%d'%(N,M,R), fontsize=20)
+    plt.title('N=%d, M=%d, R=%d, B=%d'%(N,M,R,B), fontsize=20)
     plt.grid()
     plt.xlabel('Frequency [Hz]', fontsize=20)
     plt.ylabel('Attenuation [dB]', fontsize=20)
