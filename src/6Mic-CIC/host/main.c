5ad2f26be241f5bd344fe1612d35040459a8e254
Loïc Droz <lk.droz@gmail.com>
2018-01-09 14:45:07 +0000

    Change some parameters.

--- src/6Mic-CIC/host/main.c
+++ src/6Mic-CIC/host/main.c
@@ -4,7 +4,7 @@
 #include "interface.h"
 
 #define OUTFILE "../output/interface.pcm"
-#define NSAMPLES 64000
+#define NSAMPLES 64000 * 4
 #define NCHANNELS 6
 
 int main(void) {
@@ -38,13 +38,14 @@ int main(void) {
         nanosleep(&delay, NULL);
     disable_recording();
     fflush(stdout);
+    nanosleep(&delay, NULL);
     printf("Length of the ringbuffer after recording: %zu (maxLength = %zu)\n", ringbuf_len(pcm -> main_buffer), pcm -> main_buffer -> maxLength);
 
-    pcm_read(pcm, tmp_buffer, NSAMPLES, NCHANNELS);
+    int read = pcm_read(pcm, tmp_buffer, NSAMPLES, NCHANNELS);
     pru_processing_close(pcm);
 
     printf("Outputting the results to the pcm file...\n");
-    fwrite(tmp_buffer, NCHANNELS * SAMPLE_SIZE_BYTES, NSAMPLES, outfile);
+    fwrite(tmp_buffer, NCHANNELS * SAMPLE_SIZE_BYTES, read, outfile);
 
     printf("Closing PRU processing...\n");
     fclose(outfile);
