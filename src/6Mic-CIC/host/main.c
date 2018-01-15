0b95e054b983470f7ae275db9025d07979eceab7
Loïc Droz <lk.droz@gmail.com>
2018-01-15 11:04:36 +0100

    Change rec. duration for the demo.

--- src/6Mic-CIC/host/main.c
+++ src/6Mic-CIC/host/main.c
@@ -40,7 +40,7 @@ int main(void) {
     }
 
     struct timespec delay = { 0, 250000000 };  // Wait 250 ms
-    const size_t limit = 50;
+    const size_t limit = 35;
     enable_recording();
         nanosleep(&delay, NULL);
         for (size_t i = 0; i < limit; ++i) {
